a4459f85a69ec8b658cb0a4b4ff43cf699288bb6
naim <daddinaim@gmail.com>
2022-03-13 22:02:21 +0100

    ba

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://naim2613.github.io/",
+    "name": "Daddi Naïm",
+    "role": "Intégrateur Wordpress",
+    "linkedinId":"test",
+    "skypeid": "",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/naïm-daddi/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/naim2613",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://dnweb26.fr",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"",
+    "address":"France",
+    "website":"https://dnweb26.fr",
+    "education":[
+      {
+        "UniversityName":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "specialization":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "MonthOfPassing":"janvier",
+        "YearOfPassing":"2020",
+        "Achievements":"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+      },
+      {
+        "UniversityName":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "specialization":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "specialization":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"",
+        "specialization":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"./images/portfolio/retrocam.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"./images/portfolio/retrocam.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"./images/portfolio/retrocam.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"./images/portfolio/retrocam.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"",
+        "name":""
+      },
+      {
+        "description":"",
+        "name":""
+      }
+    ]
+  }
+  
+  export default resumeData
